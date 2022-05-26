64fe16d1ce5f41d832ad6e5666ec15b924d6198e
asafedainez <asafe.sousa@gmail.com>
2022-05-26 18:08:59 -0300

    feat(controllers): implementado resposta do endpoint para todas as vendas ou por id

---
+++ controllers/sales.controllers.js
@@ -0,0 +1,12 @@
+const httpStatus = require('../utils/http/status');
+const service = require('../services/sales.services');
+
+const getAll = async (req, res) => {
+  const { id } = req.params;
+  const response = await service.getAll(id);
+  return res.status(httpStatus.OK).json(response);
+};
+
+module.exports = {
+  getAll,
+};
