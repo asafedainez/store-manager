dc262a8e034bf84fccf719c5650a717a34b40105
asafedainez <asafe.sousa@gmail.com>
2022-05-26 17:04:50 -0300

    feat(routes): implementado endpoint de produtos por id

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const controller = require('../controllers/products.controllers');
 
+routes.get('/:id', controller.getAll);
+
 routes.get('/', controller.getAll);
 
 module.exports = routes;
