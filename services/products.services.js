fa944a5e76114f4e79f9a1d3f8cb48fe0b41ff9d
asafedainez <asafe.sousa@gmail.com>
2022-05-26 16:59:29 -0300

    feat(services): implementada lógica para roda de busca de todos os produtos e por id

--- services/products.services.js
+++ services/products.services.js
@@ -1,1 +1,14 @@
-// Renomeie esse arquivo
+const model = require('../models/products.models');
+
+const getAll = async (id = null) => {
+  if (id) {
+    const response = await model.getById(id);
+    return response;
+  }
+  const response = await model.getAll();
+  return response;
+};
+
+module.exports = {
+  getAll,
+};
