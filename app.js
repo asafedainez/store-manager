8afe489f4a0c21b7328c4eb96ca93789cdc42efc
asafedainez <asafe.sousa@gmail.com>
2022-05-26 16:57:50 -0300

    feat(routes): implementada rota de produtos

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const error = require('./middlewares/error.middleware');
+const products = require('./routes/products.routes');
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', products);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
