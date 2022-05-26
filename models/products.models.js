9bb33876cdd692597306e57b3caad15ff866a3ca
asafedainez <asafe.sousa@gmail.com>
2022-05-26 16:35:26 -0300

    feat(model): implementado métodos de recuperar todos produtos ou por id

--- models/products.models.js
+++ models/products.models.js
@@ -1,1 +1,20 @@
-// Renomeie esse arquivo
+const connection = require('../utils/db/connection');
+
+const getAll = async () => {
+  const [result] = await connection.execute('select * from products');
+  return result;
+};
+
+const getById = async (id) => {
+  const [result] = await connection.execute(
+    'select * from products where id = ?',
+    [id],
+  );
+
+  return result;
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
