aad6a255d7921f705f554dc83515a75ca63b11e2
asafedainez <asafe.sousa@gmail.com>
2022-05-26 15:52:24 -0300

    test: wip

---
+++ test/unit/controllers/products.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const http_status = require('../../../utils/http/status');
+
+describe('Quando o é chamada o controller getAll', () => {
+  describe('Quando não existem dados no BD', () => {});
+});
