b8282a52ab6fd8aa3e34e1c716a80d944b5802e8
asafedainez <asafe.sousa@gmail.com>
2022-05-26 18:49:58 -0300

    fix(models): corrigido retorno de vendas por id

--- models/sales.models.js
+++ models/sales.models.js
@@ -1,11 +1,17 @@
 const connection = require('../utils/db/connection');
 
-const resultShape = (result) => ({
+const resultGetAllShape = (result) => ({
   saleId: result.sale_id,
   productId: result.product_id,
   quantity: result.quantity,
   id: result.id,
   date: result.date,
+});
+
+const resultGetByIdShape = (result) => ({
+  date: result.date,
+  productId: result.product_id,
+  quantity: result.quantity,
 });
 
 const getAll = async () => {
@@ -14,16 +20,20 @@ const getAll = async () => {
               from sales_products as sp
               inner join sales as s
               on sp.sale_id = s.id`);
-  return result.map(resultShape);
+  return result.map(resultGetAllShape);
 };
 
 const getById = async (id) => {
   const [result] = await connection.execute(
-    'select * from sales where id = ?',
+    `select s.date, sp.product_id, quantity
+    from sales as s
+    inner join sales_products as sp
+    on s.id = sp.sale_id
+    where id = ?`,
     [id],
   );
 
-  return result;
+  return result.map(resultGetByIdShape);
 };
 
 module.exports = {
