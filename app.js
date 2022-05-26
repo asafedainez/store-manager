1e145f08de9225186d4a68a902a59a4527efa569
asafedainez <asafe.sousa@gmail.com>
2022-05-26 18:04:51 -0300

    feat: implementando rota de vendas

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 require('express-async-errors');
 const error = require('./middlewares/error.middleware');
 const products = require('./routes/products.routes');
+const sales = require('./routes/sales.routes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', products);
+
+app.use('/sales', sales);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
