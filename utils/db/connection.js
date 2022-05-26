81a99abe8ed443ceece005fb14820ab024fac174
asafedainez <asafe.sousa@gmail.com>
2022-05-26 19:38:42 -0300

    fix(connection db): corrigido valor padrão para nome do BD

--- utils/db/connection.js
+++ utils/db/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',
-    database: process.env.MYSQL_DATABASE || 'model_example', 
+    database: process.env.MYSQL_DATABASE || 'StoreManager', 
   });
 
 module.exports = connection; 
