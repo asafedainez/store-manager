67566e6a03050d26588ca0f80ea194becd2f78f4
asafedainez <asafe.sousa@gmail.com>
2022-05-26 17:02:36 -0300

    feat(controllers): implementada requisição de recuperação de todos os produtos ou por id

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,1 +1,12 @@
-// Renomeie esse arquivo
+const httpStatus = require('../utils/http/status');
+const service = require('../services/products.services');
+
+const getAll = async (req, res) => {
+  const { id } = req.params;
+  const response = await service.getAll(id);
+  return res.status(httpStatus.OK).json(response);
+};
+
+module.exports = {
+  getAll,
+};
