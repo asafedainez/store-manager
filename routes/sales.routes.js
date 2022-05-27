6e5209a67ce7b818e81be9978621f2841a1e2c19
asafedainez <asafe.sousa@gmail.com>
2022-05-26 22:34:10 -0300

    feat(routes): criado rotas para teste de validação do body de requisição
    Criado rotas não funcionais de POST e PUT em vendas e produtos para validação de middleware

--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -1,8 +1,13 @@
 const routes = require('express').Router();
 const controller = require('../controllers/sales.controllers');
+const middleware = require('../middlewares/sales.middleware');
 
 routes.get('/:id', controller.getAll);
 
 routes.get('/', controller.getAll);
+
+routes.post('/', middleware, (_req, _res) => {});
+
+routes.put('/:id', middleware, (_req, _res) => {});
 
 module.exports = routes;
