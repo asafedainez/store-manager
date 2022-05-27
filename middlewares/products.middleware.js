ff505c29634a7f98ee873e73ac2b3edf49e5565d
asafedainez <asafe.sousa@gmail.com>
2022-05-26 22:29:20 -0300

    feat(middlewares): criado validação do body para endpoint de products
    Criado validação para verificar os dados trazidos no body da requisição para POST e PUT no endpoint
    de produtos

--- middlewares/products.middleware.js
+++ middlewares/products.middleware.js
@@ -1,1 +1,26 @@
-// Renomeie esse arquivo
+const joi = require('joi');
+const httpStatus = require('../utils/http/status');
+
+const validateBodyProduct = joi.object().keys({
+  name: joi.string().min(5).required(),
+  quantity: joi.number().min(1).required(),
+}).messages({
+  'any.required': `${httpStatus.BAD_REQUEST}-{{#label}} is required`,
+  'string.min': `${httpStatus.UNPROCESSABLE_ENTITY}-\
+{{#label}} length must be at least 5 characters long`,
+  'number.min': `${httpStatus.UNPROCESSABLE_ENTITY}-{{#label}} must be greater than or equal to 1`,
+});
+
+const validateProduct = (req, _res, next) => {
+  const { error } = validateBodyProduct.validate(req.body);
+
+  if (error) {
+    const [status, message] = error.details[0].message.split('-');
+    const response = { status, message };
+    throw response;
+  }
+
+  next();
+};
+
+module.exports = validateProduct;
