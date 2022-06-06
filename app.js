473a2d706e0e9adcd2b23819e9a5617668bcfcd2
asafedainez <asafe.sousa@gmail.com>
2022-06-06 12:26:14 -0300

    chore(arquivos trybe): removendo todos os arquivos pertencentes à Trybe
    Esse projeto foi uma avaliação da Trybe e para publicar em meu github pessoal preciso remover todos
    os arquivos pretencentes à Trybe

--- app.js
+++ app.js
@@ -12,14 +12,10 @@ app.use('/products', products);
 
 app.use('/sales', sales);
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.use(error);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
